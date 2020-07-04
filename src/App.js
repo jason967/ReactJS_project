c6839a50189d47047270f74e6d5182c925257e35
jason967 <jason967@naver.com>
2020-07-05 01:07:40 +0900

    EndProject

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import "./App.css";
+import Detail from "./routes/Detail";
 
 import Home from "./routes/Home";
 import About from "./routes/About";
+import Navigation from "./components/Navigation";
 
 import { HashRouter, Route } from "react-router-dom";
 
 function App() {
   return (
     <HashRouter>
+      <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/movie-detail" component={Detail} />
     </HashRouter>
   );
 }
